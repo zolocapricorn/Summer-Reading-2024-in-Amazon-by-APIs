f2021030afdf063fd3ee54ef9077955c5497e925
zolo_capricorn <suijuku@outlook.com>
2024-09-02 20:13:55 +0700

    Update book_name function and create writer_seller_kindle function.

--- request.py
+++ request.py
@@ -6,13 +6,33 @@ from pprint import pprint
 def requesting(url, head):
     """Requesting data from amazon website"""
     response = requests.get(url, headers=head)
-    # return response
-    if response:
+    if response.status_code == 200:
         return BeautifulSoup(response.text, "lxml")
+    return "Cannot requesting"
+
 
 def book_name(soup):
-    tags = soup.select_one("")
-    return tags
+    tags = soup.select("span.a-size-medium")
+    book_list = [book.text for book in tags]
+    return book_list
+
+
+def writer_seller_kindle(soup):
+    tags = soup.select("a.a-size-base")
+    writer_seller_kindle_list = [writer.text for writer in tags]
+    writer_list, seller_list, kindle_list = [], [], []
+    for count in range(len(writer_seller_kindle_list)):
+        if count%3 == 0:
+            writer_list.append(writer_seller_kindle_list[count])
+        elif count%3 == 1:
+            seller_list.append(writer_seller_kindle_list[count])
+        else:
+            if writer_seller_kindle_list[count] == "Kindle Edition":
+                kindle_list.append("Yes")
+            else:
+                kindle_list.append("No")
+    return writer_list, seller_list, kindle_list
+
 
 def main():
     url = "https://www.amazon.com/s?i=digital-text&rh=n%3A122131175011&fs=true&ref=lp_122131175011_sar"
@@ -22,11 +42,9 @@ def main():
             "Accept": "*/*", 
             "Referer": "https://www.amazon.com/"}
     soup = requesting(url, head)
-    # print(soup)
-    BookName = book_name(soup)
-    # pprint(BookName)
-    # data = soup.find("span", class_="a-truncate-full a-offscreen")
-    # print(soup.find('div'))
-    # print(data)
+    # BookName = book_name(soup)
+    WriterSellerKindle = writer_seller_kindle(soup)
+    # BookBadge = book_badge(soup)
+    print(WriterSellerKindle)
     
 main()
